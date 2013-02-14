34a832b5c859159d7dcc929941a8159f2bf2ed45
Johann Pardanaud <pardanaud.j@gmail.com>
2013-02-14 18:22:32 +0100

    The user name needs to be stored in the database class, it will be used many times

--- classes/ext_database.class.php
+++ classes/ext_database.class.php
@@ -3,11 +3,14 @@
 class Database {
 
     private $db;
+    private $user;
 
     public function __construct() {
         $this->db = $db = new PDO('mysql:host=localhost;dbname=mrbs', 'root', '');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->exec('SET NAMES UTF8');
+
+        $this->user = getUserName();
     }
 
 }
