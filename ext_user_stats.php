0b48eaa85ae6cceab996548770c9390c2a8134dd
Johann Pardanaud <pardanaud.j@gmail.com>
2013-02-14 15:12:59 +0100

    Header and footer generated by MRBS added to the dashboard

--- ext_user_stats.php
+++ ext_user_stats.php
@@ -6,4 +6,13 @@ require_once "theme.inc";
 // Checks if the user is authorised for this page
 checkAuthorised();
 
+// Prints the page header
+print_header($day, $month, $year, $area, isset($room) ? $room : "");
+
+?>
+
+<?php
+
+require_once "trailer.inc";
+
 ?>
