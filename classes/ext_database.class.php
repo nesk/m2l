050d330ed24fab409e030945770645197e9884af
Johann Pardanaud <pardanaud.j@gmail.com>
2013-02-14 18:20:20 +0100

    The database connection needs to be encoded in utf-8

--- classes/ext_database.class.php
+++ classes/ext_database.class.php
@@ -5,8 +5,9 @@ class Database {
     private $db;
 
     public function __construct() {
-        $this->db = new PDO('mysql:host=localhost;dbname=mrbs', 'root', '');
-        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->db = $db = new PDO('mysql:host=localhost;dbname=mrbs', 'root', '');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->exec('SET NAMES UTF8');
     }
 
 }
