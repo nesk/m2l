cd662d73052f60db6553bfbe61d10f3910ee22ce
Johann Pardanaud <pardanaud.j@gmail.com>
2013-02-15 13:41:46 +0100

    Actual and past reservations are displayed on the dashboard

--- ext_user_stats.php
+++ ext_user_stats.php
@@ -16,11 +16,74 @@ require_once "ext_autoloader.php";
 // Checks if the user is authorised for this page
 checkAuthorised();
 
+// Connecting to the database
+$db = new Database();
+
 // Prints the page header
 print_header($day, $month, $year, $area, isset($room) ? $room : "");
 
+/*
+ * Content
+ */
+
 ?>
 
+<h1>Tableau de bord</h1>
+
+<h2>Vos réservations actuelles</h2>
+
+<table class="admin_table">
+    <tr>
+        <th>Nom de la réservation</th>
+        <th>Nom de la salle</th>
+        <th>Début de la réunion</th>
+        <th>Fin de la réunion</th>
+    </tr>
+
+<?php $db->getActualEntries(function($data) { ?>
+    <tr>
+        <td><a href="/view_entry.php?id=<?=$data['eId']?>"><?=$data['name']?></a></td>
+        <td><a href="/edit_area_room.php?change_room=1&phase=1&room=<?=$data['rId']?>"><?=$data['room']?></a></td>
+        <td><?=date('d/m/Y H:i', $data['start'])?></td>
+        <td><?=date('d/m/Y H:i', $data['end'])?></td>
+    </tr>
+<?php }, function() { ?>
+    <tr>
+        <td colspan="4" style="text-align:center">Aucune réservation à afficher</td>
+    </tr>
+<?php }); ?>
+
+</table>
+
+<h2>Vos réservations terminées</h2>
+
+<table class="admin_table">
+    <tr>
+        <th>Nom de la réservation</th>
+        <th>Nom de la salle</th>
+        <th>Début de la réunion</th>
+        <th>Fin de la réunion</th>
+    </tr>
+
+<?php $db->getPastEntries(function($data) { ?>
+    <tr>
+        <td><a href="/view_entry.php?id=<?=$data['eId']?>"><?=$data['name']?></a></td>
+        <td><a href="/edit_area_room.php?change_room=1&phase=1&room=<?=$data['rId']?>"><?=$data['room']?></a></td>
+        <td><?=date('d/m/Y H:i', $data['start'])?></td>
+        <td><?=date('d/m/Y H:i', $data['end'])?></td>
+    </tr>
+<?php }, function() { ?>
+    <tr>
+        <td colspan="2" style="text-align:center">Aucune réservation à afficher</td>
+    </tr>
+<?php }); ?>
+
+</table>
+
 <?php
+
+/*
+ * Footer
+ */
 
 require_once "trailer.inc";
