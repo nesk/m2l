b609e54ee301acad6ca8ccdd91a91250eaa2a720
Johann Pardanaud <pardanaud.j@gmail.com>
2013-02-22 08:58:14 +0100

    Statistic function entriesNb() created

--- classes/ext_database.class.php
+++ classes/ext_database.class.php
@@ -85,6 +85,41 @@ class Database {
         }
     }
 
+    // groupby is the name of the MySQL function used to group the data
+    private function entriesNb($start, $end, $period, $onData) {
+        $sqlDate = 'from_unixtime(start_time)';
+        
+        switch($period) {
+            case 'day':
+                $groupby = 'dayofyear('. $sqlDate .')';
+                break;
+            case 'week': // Check https://dev.mysql.com/doc/refman/5.5/en/date-and-time-functions.html#function_week
+                $groupby = 'week('. $sqlDate .', 3)';
+                break;
+            case 'month':
+                $groupby = 'month('. $sqlDate .')';
+                break;
+        }
+
+        $req = $this->db->prepare('
+            SELECT count(id) AS nb, start_time AS date
+            FROM mrbs_entry
+            WHERE create_by = :user
+            AND (start_time <= :end OR end_time >= :start)
+            GROUP BY '. $groupby .'
+        ');
+
+        $req->execute(array(
+            'user' => $this->user,
+            'start' => $start,
+            'end' => $end + 86399 // Going to the last second of the specified day
+        ));
+
+        while($data = $req->fetch()) {
+            $onData($data);
+        }
+    }
+
     public function getStats($type, $options, $onData) {
         $o = $options; // Shortcut
 
@@ -92,6 +127,10 @@ class Database {
             case 'roomUsage':
                 $this->roomUsage($o['start'], $o['end'], $onData);
                 break;
+            case 'entriesNb':
+                $this->entriesNb($o['start'], $o['end'], $o['period'], $onData);
+                break;
         }
     }
+
 }
