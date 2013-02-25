8f0040b96fdafdaa098522d0993d5db7d79d066e
Johann Pardanaud <pardanaud.j@gmail.com>
2013-02-25 22:58:02 +0100

    Client-side script used to retrieve and display stats is created

--- ext_dashboard.js
+++ ext_dashboard.js
@@ -30,4 +30,82 @@
 
     form.find('[name="chart-start"], [name="chart-end"]').datepicker();
 
+    /*
+     * XHR and charts
+     */
+
+    google.load('visualization', '1', {packages:['corechart']});
+
+    google.setOnLoadCallback(function() {
+
+        
+
+        /*
+         * Chart types
+         *
+         * Each chart type is an object containing settings used to generate
+         * the XHR and the appropriate chart.
+         */
+
+        var chartTypes = {
+
+            roomUsage: {
+                object: google.visualization.PieChart
+            },
+
+            entriesNb: {
+                object: google.visualization.LineChart,
+                additionalInputs: ['chart-period']
+            }
+
+        };
+
+        /*
+         * Bindings and XHR
+         */
+
+        form.find('[type="submit"]').click(function(event) {
+
+            event.preventDefault();
+
+            var selectedChart = form.find('select[name="chart-type"] option:selected'),
+                chartType = chartTypes[selectedChart.attr('value')],
+                data = {};
+            
+            // If there's no additional input, we create the property. In the two cases, we add "chart-start" and "chart-end".
+            chartType.additionalInputs = chartType.additionalInputs || [];
+            chartType.additionalInputs.push('chart-start', 'chart-end');
+
+            // Retrieves all the data (including "chart-start" and "chart-end") and stores it in an object
+            for(var i = 0, input ; input = chartType.additionalInputs[i++] ;) {
+                data[input] = encodeURIComponent(form.find('[name="'+ input +'"]').attr('value'));
+            }
+
+            // Retrieves the user of the dashboard and adds it to the data
+            data.user = $('#dashboard-user option:selected').attr('value');
+
+            $.ajax({
+                url: '/ext_charts_xhr.php',
+                dataType: 'json',
+                data: data,
+
+                success: function(data) {
+                    var chart = new chartType.object(document.getElementById('chart'));
+
+                    data = google.visualization.arrayToDataTable(data);
+
+                    // Some options need to be rewritten to avoid visual issues
+                    var options = chartType.options = chartType.options || {};
+                    options.title = selectedChart.html();
+                    options.backgroundColor = '#e7ebee';
+                    options.height = 400;
+
+                    chart.draw(data, options);
+                }
+            });
+
+        });
+
+    });
+
 })();
